eff14929efcb62bf3d52027c2e7c913229b6f152
IAmDirt <33249694+IAmDirt@users.noreply.github.com>
2022-01-28 10:01:46 +0100

    changed max light per scene

---
+++ Assets/SoftFlicker.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class SoftFlicker : MonoBehaviour
+{
+    private float minIntensity =0.7f;
+    private float maxIntensity = 1.3f;
+    private float startIntensity;
+    private float random;
+    private Light light;
+    void Start()
+    {
+        light = GetComponent<Light>();
+        startIntensity = light.intensity;
+        random = Random.Range(0.0f, 65535.0f);
+    }
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time *2);
+        light.intensity = Mathf.Lerp(startIntensity *minIntensity, startIntensity*maxIntensity, noise);
+    }
+}
